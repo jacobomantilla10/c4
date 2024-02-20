9fba9790a54279c1b4a8873a02fdf00d40b48f06
jacobo mantilla <jacobomantilla10@gmail.com>
2024-02-20 08:43:31 -0600

    Changed the board to be composed of runes. Adjusted makeBoard to initialize empty slots to ' ' rune. Created Turn struct to hold the player Id and the character corresponding to it. Added conditional printing depending on if the game was won or drawn.

--- main.go
+++ main.go
@@ -5,62 +5,82 @@ import "fmt"
 func main() {
 	board := makeBoard()
 	board.DrawBoard()
-	turn := 2
+	turn := makeTurn()
 
 	// Add turns by adding players
-	isOver := false
+	isWin := false
+	isDraw := false
+	isOver := isWin || isDraw
 	for !isOver {
-		if turn == 2 {
-			turn = 1
+		if turn.playerId == 2 {
+			turn.playerId = 1
+			turn.character = 'X'
 		} else {
-			turn = 2
+			turn.playerId = 2
+			turn.character = 'O'
 		}
 		// start a players turn and depending on whose turn it is paint different symbols on the screen
-		fmt.Printf("\033[2K\rEnter column player %d: ", turn)
+		fmt.Printf("\033[2K\rEnter column player %d: ", turn.playerId)
 		var col int
 		fmt.Scanln(&col)
 		// Insert checker into col
-		err := board.InsertIntoCol(col-1, turn)
+		err := board.InsertIntoCol(col-1, turn.character)
 		for err != nil {
 			fmt.Print("\033[1A\033[2K")
-			fmt.Printf("\rInvalid insert... Enter column player %d: ", turn)
+			fmt.Printf("\rInvalid insert... Enter column player %d: ", turn.playerId)
 			fmt.Scanln(&col)
-			err = board.InsertIntoCol(col-1, turn)
+			err = board.InsertIntoCol(col-1, turn.character)
 		}
-		isWin := board.IsWin()
-		isDraw := board.IsDrawn()
+		isWin = board.IsWin()
+		isDraw = board.IsDrawn()
 		isOver = isWin || isDraw
 		fmt.Print("\033[15A")
 		board.DrawBoard()
 	}
-	fmt.Printf("\033[2K\rGame is over. Player %d wins!\n", turn)
+	if isWin {
+		fmt.Printf("\033[2K\rGame is over. Player %d wins!\n", turn.playerId)
+	} else {
+		fmt.Printf("\033[2K\rGame is a draw.")
+	}
+}
+
+type Turn struct {
+	playerId  int
+	character rune
+}
+
+func makeTurn() Turn {
+	return Turn{2, 'O'}
 }
 
 type Board struct {
 	w, h int
-	data [6][7]int
+	data [6][7]rune
 }
 
 func makeBoard() Board {
-	arr := [6][7]int{}
+	arr := [6][7]rune{}
+	for i := range arr {
+		arr[i] = [7]rune{' ', ' ', ' ', ' ', ' ', ' ', ' '}
+	}
 	return Board{6, 7, arr}
 }
 
-func (b *Board) At(x, y int) int {
+func (b *Board) At(x, y int) rune {
 	return b.data[x][y]
 }
 
-func (b *Board) Set(x, y, new int) {
+func (b *Board) Set(x, y int, new rune) {
 	b.data[x][y] = new
 }
 
-func (b *Board) InsertIntoCol(y, checker int) error {
+func (b *Board) InsertIntoCol(y int, checker rune) error {
 	if y > len(b.data) || y < 0 {
 		return fmt.Errorf("invalid insert")
 	}
 
 	for x := len(b.data) - 1; x >= 0; x-- {
-		if b.data[x][y] == 0 {
+		if b.data[x][y] == 32 {
 			b.Set(x, y, checker)
 			return nil
 		}
@@ -73,7 +93,7 @@ func (b *Board) DrawBoard() {
 	for i := range b.data {
 		fmt.Printf("\033[2K+---+---+---+---+---+---+---+\n")
 		for j := range b.data[i] {
-			fmt.Printf("| %d ", b.data[i][j])
+			fmt.Printf("| %c ", b.data[i][j])
 		}
 		fmt.Printf("|\n")
 	}
@@ -81,12 +101,14 @@ func (b *Board) DrawBoard() {
 }
 
 func (b *Board) IsWin() bool {
+	// Figure out how to check for empty runes
+	// What is the nil value for a rune?
 	for x := len(b.data) - 1; x >= 0; x-- {
 		// C is the index of the first occurrence of a non-empty checker in a horizontal
 		c := 0
 		for y := range b.data[x] {
 			// Check horizontal
-			if b.data[x][y] != b.data[x][c] || b.data[x][y] == 0 {
+			if b.data[x][y] != b.data[x][c] || b.data[x][y] == 32 {
 				c = y
 			}
 			if y-c >= 3 {
@@ -94,7 +116,7 @@ func (b *Board) IsWin() bool {
 			}
 			// Check vertical
 			h := x - 1
-			for h >= 0 && b.data[h][y] == b.data[x][y] && b.data[h][y] != 0 {
+			for h >= 0 && b.data[h][y] == b.data[x][y] && b.data[h][y] != 32 {
 				h--
 				if x-h == 4 {
 					return true
@@ -103,7 +125,7 @@ func (b *Board) IsWin() bool {
 			// Check left up diagonal
 			h = x - 1
 			w := y - 1
-			for h >= 0 && w >= 0 && b.data[h][w] == b.data[x][y] && b.data[h][w] != 0 {
+			for h >= 0 && w >= 0 && b.data[h][w] == b.data[x][y] && b.data[h][w] != 32 {
 				h--
 				w--
 				if x-h == 4 {
@@ -113,7 +135,7 @@ func (b *Board) IsWin() bool {
 			// Check up right diagonal
 			h = x - 1
 			w = y + 1
-			for h >= 0 && w < len(b.data[x]) && b.data[h][w] == b.data[x][y] && b.data[h][w] != 0 {
+			for h >= 0 && w < len(b.data[x]) && b.data[h][w] == b.data[x][y] && b.data[h][w] != 32 {
 				h--
 				w++
 				if x-h == 4 {
@@ -128,7 +150,7 @@ func (b *Board) IsWin() bool {
 func (b *Board) IsDrawn() bool {
 	for x := range b.data {
 		for y := range b.data[x] {
-			if b.data[x][y] == 0 {
+			if b.data[x][y] == 32 {
 				return false
 			}
 		}
